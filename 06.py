f517407c4c17419f59a4da2e5e92a481563ccf5e
Animesh <134920175+AnimeshMalhans@users.noreply.github.com>
2024-03-22 04:41:08 -0400

    Day 6
    This is day 6 of python code challenge

---
+++ 06.py
@@ -0,0 +1,19 @@
+name = "abhi"
+friend = "baba"
+anotherfriend = "baba 2" 
+print("Your friends list: "+ friend +  anotherfriend)
+
+print('He said,' "I want to eat an apple")
+
+whathesaid = "he said,"
+apple = "he want to eat an apple"
+print(whathesaid + apple)
+
+name =  '''abhi ek bdiya baccha he'''
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print("Lets use a for loop")
+for character in name:
+    print(character)
